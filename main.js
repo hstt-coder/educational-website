a96620faa0095ee562335dd1571587188f1f7dfc
hstt-coder <eduardolopes19@outlook.com>
2023-01-11 21:51:21 -0300

    FAQ Section

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle(
     'window-scroll', window.scrollY > 0);
 })
+
+// toggle faq answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    //change icon
+    const icon = faq.querySelector('.faq__icon i');
+    icon.className = icon.className === "uil uil-plus" ? "uil uil-minus" : "uil uil-plus"
+  })
+});
